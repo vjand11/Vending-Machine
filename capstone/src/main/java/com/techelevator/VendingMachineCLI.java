2478d67d3a3ee80328a8357f3ba733187ad5eba1
Virginia Anderson <virginia.j.schmidt@gmail.com>
2022-02-10 16:07:20 -0500

    item code menu updates

--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -6,84 +6,87 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.sql.SQLOutput;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class VendingMachineCLI {
 
-	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
-	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
-	private static final String MAIN_MENU_EXIT = "Exit";
-	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_EXIT};
-	private static final String PURCHASE_MENU_OPTION_FEED_MONEY = "Feed Money";
-	private static final String PURCHASE_MENU_OPTION_SELECT_PRODUCT = "Select Product";
-	private static final String PURCHASE_MENU_OPTION_FINISH_TRANSACTION = "Finish Transaction";
-	private static final String[] PURCHASE_MENU_OPTIONS ={PURCHASE_MENU_OPTION_FEED_MONEY, PURCHASE_MENU_OPTION_SELECT_PRODUCT, PURCHASE_MENU_OPTION_FINISH_TRANSACTION};
-	private static final String DOLLAR = "1.00";
-	private static final String TWO_DOLLARS = "2.00";
-	private static final String FIVE_DOLLARS = "5.00";
-	private static final String TEN_DOLLARS = "10.00";
-	private static final String[] FEED_MONEY_MENU_OPTIONS = {DOLLAR, TWO_DOLLARS, FIVE_DOLLARS, TEN_DOLLARS};
+    private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
+    private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
+    private static final String MAIN_MENU_EXIT = "Exit";
+    private static final String[] MAIN_MENU_OPTIONS = {MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_EXIT};
+    private static final String PURCHASE_MENU_OPTION_FEED_MONEY = "Feed Money";
+    private static final String PURCHASE_MENU_OPTION_SELECT_PRODUCT = "Select Product";
+    private static final String PURCHASE_MENU_OPTION_FINISH_TRANSACTION = "Finish Transaction";
+    private static final String[] PURCHASE_MENU_OPTIONS = {PURCHASE_MENU_OPTION_FEED_MONEY, PURCHASE_MENU_OPTION_SELECT_PRODUCT, PURCHASE_MENU_OPTION_FINISH_TRANSACTION};
+    private static final String DOLLAR = "1.00";
+    private static final String TWO_DOLLARS = "2.00";
+    private static final String FIVE_DOLLARS = "5.00";
+    private static final String TEN_DOLLARS = "10.00";
+    private static final String[] FEED_MONEY_MENU_OPTIONS = {DOLLAR, TWO_DOLLARS, FIVE_DOLLARS, TEN_DOLLARS};
+    private static final String[] ITEM_CODES = {"A1 - Potato Crisps", "A2 - Stackers", "A3 - Grain Waves",
+            "A4 - Cloud Popcorn", "B1 - Moonpie", "B2 - Cowtales", "B3 - Wonka Bar", "B4 - Crunchie", "C1 - Cola",
+            "C2 - Dr. Salt", "C3 - Mountain Melter", "C4 - Heavy", "D1 - U-Chews", "D2 - Little League Chew", "D3 - Chiclets", "D4 - Triplemint"};
 
 
-	private VendingMachine vendingMachine = new VendingMachine();
+    private VendingMachine vendingMachine = new VendingMachine();
 
-	private Menu menu;
+    private Menu menu;
 
-	public VendingMachineCLI(Menu menu) {
-		this.menu = menu;
-	}
+    public VendingMachineCLI(Menu menu) {
+        this.menu = menu;
+    }
 
-	public void run() {
+    public void run() {
 
-		while (true) {
-			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
+        while (true) {
+            String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 
-			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
-				// display vending machine items
-				String vendingItems = "";
+            if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
+                // display vending machine items
+                String vendingItems = "";
 
-				try (Scanner inputFile = new Scanner(new File("vendingmachine.csv"))) {
-					while (inputFile.hasNextLine()) {
-						vendingItems = inputFile.nextLine();
-						System.out.println(vendingItems);
-					}
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
-				//Menu purchaseMenu = new Menu(System.in, System.out);
+                try (Scanner inputFile = new Scanner(new File("vendingmachine.csv"))) {
+                    while (inputFile.hasNextLine()) {
+                        vendingItems = inputFile.nextLine();
+                        System.out.println(Arrays.toString(vendingItems.split("\\|")));
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            } else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 
-				while (true) {
-					//Balance startingBalance = new Balance(0.00);
+                while (true) {
 
-					System.out.println("Current Money Provided: " + vendingMachine.getVendingBalance().getBalance());
-					String purchaseMenuChoice = (String) this.menu.getChoiceFromOptions(PURCHASE_MENU_OPTIONS);
+                    System.out.println("Current Money Provided: " + vendingMachine.getVendingBalance().getBalance());
+                    String purchaseMenuChoice = (String) this.menu.getChoiceFromOptions(PURCHASE_MENU_OPTIONS);
 
-					if (purchaseMenuChoice.equals(PURCHASE_MENU_OPTION_FEED_MONEY)) {
-						System.out.print("How much money would you like to add? ");
-						String feedMoneyMenuChoice = (String) this.menu.getChoiceFromOptions(FEED_MONEY_MENU_OPTIONS);
+                    if (purchaseMenuChoice.equals(PURCHASE_MENU_OPTION_FEED_MONEY)) {
+                        System.out.print("How much money would you like to add? ");
+                        String feedMoneyMenuChoice = (String) this.menu.getChoiceFromOptions(FEED_MONEY_MENU_OPTIONS);
 
-						vendingMachine.getVendingBalance().addMoney(Double.parseDouble(feedMoneyMenuChoice));
+                        vendingMachine.getVendingBalance().addMoney(Double.parseDouble(feedMoneyMenuChoice));
 
-					} else if (purchaseMenuChoice.equals(PURCHASE_MENU_OPTION_SELECT_PRODUCT)) {
+                    } else if (purchaseMenuChoice.equals(PURCHASE_MENU_OPTION_SELECT_PRODUCT)) {
 
-						System.out.println("Enter items code: ");
-						Scanner input = new Scanner(System.in);
-						String itemCode = input.next();
+                        System.out.println("Enter items code: ");
+                        String itemCodeChoice = (String) this.menu.getChoiceFromOptions(ITEM_CODES);
+                        boolean itemSelected = vendingMachine.returnItems(itemCodeChoice);
+                        System.out.println(itemSelected);
 
-					} else {
-						break;
-					}
-				}
-			} else {
-				System.exit(0);
-			}
-		}
-	}
+                    } else {
+                        break;
+                    }
+                }
+            } else {
+                System.exit(0);
+            }
+        }
+    }
 
-	public static void main(String[] args) {
-		Menu menu = new Menu(System.in, System.out);
-		VendingMachineCLI cli = new VendingMachineCLI(menu);
-		cli.run();
-	}
+    public static void main(String[] args) {
+        Menu menu = new Menu(System.in, System.out);
+        VendingMachineCLI cli = new VendingMachineCLI(menu);
+        cli.run();
+    }
 }
--- capstone/src/main/java/com/techelevator/view/VendingMachine.java
+++ capstone/src/main/java/com/techelevator/view/VendingMachine.java
@@ -1,6 +1,11 @@
 package com.techelevator.view;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class VendingMachine {
+
+    private Map<String, Double> goodsMap = new HashMap<>();
 
     private Balance vendingBalance = new Balance(0.00);
 
@@ -16,4 +21,31 @@ public class VendingMachine {
 
     //create method that accepts slot code and return the map at that string
 
+
+    public Map<String, Double> getGoodsMap() {
+        goodsMap.put("Potato Crisps", 3.05);
+        goodsMap.put("Stackers", 1.45);
+        goodsMap.put("Grain Waves", 2.75);
+        goodsMap.put("Cloud Popcorn", 3.65);
+        goodsMap.put("Moonpie", 1.80);
+        goodsMap.put("Cowtales", 1.50);
+        goodsMap.put("Wonka Bar", 1.50);
+        goodsMap.put("Crunchie", 1.75);
+        goodsMap.put("Cola", 1.25);
+        goodsMap.put("Dr. Salt", 1.50);
+        goodsMap.put("Mountain Melter", 1.50);
+        goodsMap.put("Heavy", 1.50);
+        goodsMap.put("U-Chews", .85);
+        goodsMap.put("Little League Chew", .95);
+        goodsMap.put("Chiclets", .75);
+        goodsMap.put("Triplemint", .75);
+        return goodsMap;
+    }
+
+    public boolean returnItems (String slot) {
+        if (slot.contains("A1")) {
+            return goodsMap.containsKey("Potato Crisps");
+        }
+        else return false;
+    }
 }
